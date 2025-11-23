ad256bd1ad140685fda06769f3a82572ee14dd3c
Palani Akash <palaniakash1@gmail.com>
2025-11-23 23:20:17 +0530

    installed react js and tailwind css and create first template

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import  Home   from "./pages/home";
+import About from "./pages/about";
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signUp";
+import Profile from "./pages/profile";
+
+
+
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>;
+}
