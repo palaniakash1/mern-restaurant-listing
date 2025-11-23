dc5ae203137f35f2f2b39dea505241333d935a27
Palani Akash <palaniakash1@gmail.com>
2025-11-23 23:24:25 +0530

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
       <Route path="/profile" element={<Profile />} />
+    
     </Routes>
   </BrowserRouter>;
 }
