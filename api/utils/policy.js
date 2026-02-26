d4bd9d727c894052ff8de5bba372808655185987
Palani Akash <palaniakash1@gmail.com>
2026-02-26 18:29:21 +0530

    feat: add canAny function for multiple action permission checks

--- api/utils/policy.js
+++ api/utils/policy.js
@@ -18,3 +18,25 @@ export const can = (action, resource) => {
     next();
   };
 };
+
+export const canAny = (actions, resource) => {
+  return (req, res, next) => {
+    const role = req.user?.role;
+
+    if (!role) {
+      return next(errorHandler(401, "Unauthorized"));
+    }
+
+    const allowed = PERMISSIONS[role]?.[resource];
+    if (!Array.isArray(allowed)) {
+      return next(errorHandler(403, "Permission denied"));
+    }
+
+    const hasPermission = actions.some((action) => allowed.includes(action));
+    if (!hasPermission) {
+      return next(errorHandler(403, "Permission denied"));
+    }
+
+    next();
+  };
+};
