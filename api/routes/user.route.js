72bc732a9a1607e9d96562582932d0d43baf08f1
Palani Akash <palaniakash1@gmail.com>
2026-02-26 23:44:10 +0530

    feat: add validation to user routes for query and body parameters

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -15,32 +15,56 @@ import {
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { can, canAny } from "../utils/policy.js";
+import { validate } from "../middlewares/validate.js";
+import { userValidators } from "../validators/index.js";
 
 const router = express.Router();
 
 router.get("/test", verifyToken, can("test", "user"), test);
 
-router.get("/", verifyToken, can("listAll", "user"), getAllUsers);
-router.get("/admins", verifyToken, can("listAdmins", "user"), getAvailableAdmins);
+router.get(
+  "/",
+  verifyToken,
+  can("listAll", "user"),
+  validate(userValidators.listQuery, "query"),
+  getAllUsers,
+);
+router.get(
+  "/admins",
+  verifyToken,
+  can("listAdmins", "user"),
+  validate(userValidators.listQuery, "query"),
+  getAvailableAdmins,
+);
 router.get(
   "/store-managers",
   verifyToken,
   can("listStoreManagers", "user"),
+  validate(userValidators.storeManagersQuery, "query"),
   getStoreManagers,
 );
 
-router.post("/", verifyToken, can("createStoreManager", "user"), createStoreManager);
+router.post(
+  "/",
+  verifyToken,
+  can("createStoreManager", "user"),
+  validate(userValidators.createStoreManager),
+  createStoreManager,
+);
 
 router.patch(
   "/:id",
   verifyToken,
   canAny(["updateAny", "updateSelf"], "user"),
+  validate(userValidators.idParam, "params"),
+  validate(userValidators.updateUser),
   updateUser,
 );
 router.delete(
   "/:id",
   verifyToken,
   canAny(["deleteAny", "deleteSelf"], "user"),
+  validate(userValidators.idParam, "params"),
   deleteUser,
 );
 
@@ -48,21 +72,32 @@ router.patch(
   "/:id/deactivate",
   verifyToken,
   canAny(["deactivateAny", "deactivateSelf"], "user"),
+  validate(userValidators.idParam, "params"),
   deactivateUser,
 );
 
-router.patch("/:id/restore", verifyToken, can("restoreAny", "user"), restoreUser);
+router.patch(
+  "/:id/restore",
+  verifyToken,
+  can("restoreAny", "user"),
+  validate(userValidators.idParam, "params"),
+  restoreUser,
+);
 
 router.patch(
   "/:id/restaurant",
   verifyToken,
   can("assignStoreManager", "user"),
+  validate(userValidators.idParam, "params"),
+  validate(userValidators.assignStoreManager),
   assignStoreManagerToRestaurant,
 );
 router.patch(
   "/:id/owner",
   verifyToken,
   can("changeStoreManagerOwner", "user"),
+  validate(userValidators.idParam, "params"),
+  validate(userValidators.changeStoreManagerOwner),
   changeStoreManagerOwner,
 );
 
@@ -70,6 +105,7 @@ router.delete(
   "/:id/restaurant",
   verifyToken,
   can("unassignStoreManager", "user"),
+  validate(userValidators.idParam, "params"),
   unassignStoreManager,
 );
 
