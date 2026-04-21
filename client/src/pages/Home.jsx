7a95b7c947f269935c70e12d61153fd52e7c421f
Palani Akash <palaniakash1@gmail.com>
2026-04-21 16:13:31 +0530

    refactor(home): reorder featured sections and clean up whitespace
    - Move FeaturedByCategory section above NearbyRestaurants for better content flow
    - Remove trailing whitespace and add missing newline at end of file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,27 +14,27 @@ export default function Home() {
   return (
     <PageWrapper className={publicShellClass}>
       <CinematicBanner />
-      
-      <div className="my-16">
-        <NearbyRestaurants />
-      </div>
-      
-      <div className="my-16">
-        <FeaturedRestaurants />
-      </div>
-      
-      <div className="my-16">
-        <TrendingRestaurants />
-      </div>
-      
-      <div className="my-16">
-        <FeaturedByMenu />
-      </div>
-      
+
       <div className="my-16">
         <FeaturedByCategory />
       </div>
-      
+
+      <div className="my-16">
+        <NearbyRestaurants />
+      </div>
+
+      <div className="my-16">
+        <FeaturedRestaurants />
+      </div>
+
+      <div className="my-16">
+        <TrendingRestaurants />
+      </div>
+
+      <div className="my-16">
+        <FeaturedByMenu />
+      </div>
+
       <div className="my-16">
         <FsaRatingSection />
       </div>
